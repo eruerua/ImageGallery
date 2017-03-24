54bdf15b609a4f24830c25fb66e28e2cfe8f1e41
eruera <eruerame@gmail.com>
2017-03-24 13:39:27 +0800

    some fixed

---
+++ js/demo.js
@@ -0,0 +1,78 @@
+function showPic(whichpic) {
+    if (!document.getElementById("placeholder")) return false;
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    if (placeholder.nodeName != "IMG") return false;
+    placeholder.setAttribute("src",source);
+    if (document.getElementById("description")) {
+        var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title"):"";
+        var description = document.getElementById("description");
+        // alert(description.firstChild.nodeValue);
+        if (description.firstChild.nodeType == 3) {
+            description.firstChild.nodeValue = text;
+        }
+        description.firstChild.nodeValue = text;
+    }
+    return true;
+
+}
+
+// function countBodyChildren() {
+//     var body_element = document.getElementsByTagName("body")[0];
+//     alert(body_element.childNodes.length);
+// }
+
+// window.onload = countBodyChildren;
+// window.onload = function() {
+//     if (!document.getElementsByTagName) return false;
+//     var links = document.getElementsByTagName("a");
+//     console.log(links);
+//     for (var i = 0; i<links.length;i++) {
+//     if(links[i].getAttribute("class")=="popup") {
+//         links[i].onclick = function() {
+//             popUp(this.getAttribute('href'));
+//             return false;
+//         };
+//     }
+//     }
+// };
+
+// function popUp(winURL) {
+//     window.open(winURL,"popup","width=320,height=480");
+// }
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) {
+        return false;
+    }
+    if (!document.getElementById) {
+        return false;
+    }
+    if (!document.getElementById("imagegallery")) {
+        return false;
+    }
+
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function() {
+            return !showPic(this);
+        };
+    }
+
+}
+
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    }else {
+        window.onload = function() {
+            oldonload();
+            func();
+        };
+    }
+}
+
+addLoadEvent(prepareGallery);
