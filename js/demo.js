6e6743b00628aba7330e176273836f4e22f67613
eruera <eruerame@gmail.com>
2017-03-26 18:56:29 +0800

    dynamic create marks

--- js/demo.js
+++ js/demo.js
@@ -75,4 +75,37 @@ function addLoadEvent(func) {
     }
 }
 
+function insertAfter(newElement,targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {
+        parent.appendChild(newElement);
+    } else {
+        parent.insertBefore(newElement,targetElement.nextSibling);
+    }
+}
+
+function preparePlaceholder() {
+    if (!document.createElement) return false;
+    console.log("0");
+    if (!document.createTextNode) return false;
+    console.log("1");
+    if (!document.getElementById) return false;
+    console.log("2");
+    if (!document.getElementById("imagegallery")) return false;
+
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id","placeholder");
+    placeholder.setAttribute("src","images/placehold.gif");
+    placeholder.setAttribute("alt","my image gallery");
+    var description = document.createElement("p");
+    description.setAttribute("id","description");
+    var desctext = document.createTextNode("Choose an image");
+    description.appendChild(desctext);
+    var gallery = document.getElementById("imagegallery");
+    insertAfter(placeholder,gallery);
+    insertAfter(description,placeholder);
+    console.log(placeholder);
+}
+
+addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
